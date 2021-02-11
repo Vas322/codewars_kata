9e6603b9981fe146357738daac28782da98ca9cd
Vasily Dmitrochenko <dmitrochenko.vasiliy@gmail.com>
2021-02-11 23:49:33 +0300

    Update func create number

--- create_phone_number.py
+++ create_phone_number.py
@@ -6,15 +6,12 @@ create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890
 The returned format must be correct in order to complete this challenge.
 Don't forget the space after the closing parentheses!
 """
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 
 def create_phone_number(n):
-    slice1, slice2, slice3 = n[:3], n[3:6], n[6:]
-    str1 = ''.join(str(i) for i in slice1)
-    str2 = ''.join(str(i) for i in slice2)
-    str3 = ''.join(str(i) for i in slice3)
-    return f'({str1}) {str2}-{str3}'
+    n = ''.join(str(i) for i in n)
+    return f'({n[:3]}) {n[3:6]}-{n[6:]}'
 
 
-print(create_phone_number(list))
+print(create_phone_number(num_list))

61bccf52ba2883aa9880555972ea5fe3f22f9d48
Vasily Dmitrochenko <dmitrochenko.vasiliy@gmail.com>
2021-02-27 12:42:26 +0300

    update func first_non_repeating_letter

--- first_non_repeating_letter.py
+++ first_non_repeating_letter.py
@@ -17,6 +17,3 @@ string ("") or None -- see sample tests.
 def first_non_repeating_letter(string):
     string_list = [item.lower() for item in string]
     return [string[i] for i in range(len(string_list)) if string_list.count(string_list[i]) == 1]
-
-
-print(first_non_repeating_letter('dsssdsffuU'))
